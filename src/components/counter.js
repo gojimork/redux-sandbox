00c1b0e809b1989ee1ec92dead317acb4d8cabb0
gojimork <crusejohen@gmail.com>
2023-03-06 00:01:44 +0300

    connect

---
+++ src/components/counter.js
@@ -0,0 +1,27 @@
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+const Counter = ({ counter, dec, inc, rnd }) => {
+  return (
+    <div className="jumbotron">
+      <h2 id="counter">{counter}</h2>
+      <button onClick={dec} id="dec" className="btn btn-primary btn-lg">
+        DEC
+      </button>
+      <button onClick={inc} id="inc" className="btn btn-primary btn-lg">
+        INC
+      </button>
+      <button onClick={rnd} id="rnd" className="btn btn-primary btn-lg">
+        RND
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state,
+  };
+};
+
+export default connect(mapStateToProps, actions)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+import App from "./components/app";
+
+const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
